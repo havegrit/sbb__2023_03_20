80c0afd80593c829b8613be2e1336eb5919f7934
havegrit <shin_jaean@naver.com>
2023-03-20 16:06:58 +0900

    Question Entity에 answerList(@OneToMany) 필드 추가

--- src/main/java/com/mysite/sbb/Question.java
+++ src/main/java/com/mysite/sbb/Question.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
@@ -18,4 +19,6 @@ public class Question {
     @Column(columnDefinition = "TEXT")
     private String content;
     private LocalDateTime create_date;
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
 }
