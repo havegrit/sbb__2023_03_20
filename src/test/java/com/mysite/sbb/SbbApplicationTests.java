4b44a9b38751cd6435683b5b73996c24865383c4
havegrit <shin_jaean@naver.com>
2023-03-20 16:31:20 +0900

    SQL select

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -51,4 +51,24 @@ class SbbApplicationTests {
 			assertEquals("sbb가 무엇인가요?", q.getSubject());
 		}
 	}
+	@Test
+	@DisplayName("findBySubject")
+	void t004() {
+		Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+		assertEquals(1, q.getId());
+	}
+	@Test
+	@DisplayName("findBySubjectAndContent")
+	void t005() {
+		Question q = this.questionRepository.findBySubjectAndContent(
+				"sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+		assertEquals(1, q.getId());
+	}
+	@Test
+	@DisplayName("findBySubjectLike")
+	void t006() {
+		List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+		Question q = qList.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
+	}
 }
