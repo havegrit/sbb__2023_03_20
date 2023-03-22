ab9e52d97a286b5d6411009941fba8c9ddf13d98
havegrit <shin_jaean@naver.com>
2023-03-22 11:33:45 +0900

    질문 상세 링크 추가

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,7 +1,6 @@
 package com.mysite.sbb.question;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +10,6 @@ import java.util.List;
 @Controller
 @RequiredArgsConstructor
 public class QuestionController {
-    private final QuestionRepository questionRepository;
     private final QuestionService questionService;
     @GetMapping("/question/list")
     public String list(Model model) {
